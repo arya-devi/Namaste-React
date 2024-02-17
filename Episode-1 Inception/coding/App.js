a7e72263084fa8e27953255b194ff03f679222ab
Arya Devi <aryaarya96016@gmail.com>
2024-02-17 13:05:43 +0530

    how we create nested html elements using react

--- Episode-1 Inception/coding/App.js
+++ Episode-1 Inception/coding/App.js
@@ -1,10 +1,33 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React !"
+/*
+<div id="parent">
+<div id="child">
+<h1>Hello world from React</h1>
+
+</div>
+
+</div>
+
+React.createElement (object) => HTML(browser understand)
+
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Hello world from React")
+  )
 );
-console.log(heading); //this is an object not html tag yet !
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from React !"
+// );
+ console.log(parent); //this is an object not html tag yet !
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); // The render method take the react element that is a javascript object and it convert that into browser understands tags(HTML)
+root.render(parent); // The render method take the react element that is a javascript object and it convert that into browser understands tags(HTML)
