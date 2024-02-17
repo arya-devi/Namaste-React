014f47426b93cac7633d282203207e3e223d6624
Arya Devi <aryaarya96016@gmail.com>
2024-02-17 13:19:42 +0530

    how do we create siblings html elements in react

--- Episode-1 Inception/coding/App.js
+++ Episode-1 Inception/coding/App.js
@@ -1,7 +1,8 @@
 /*
 <div id="parent">
 <div id="child">
-<h1>Hello world from React</h1>
+<h1>Hello world from React h1</h1>
+<h2>Hello world from React h2</h2>
 
 </div>
 
@@ -14,11 +15,10 @@ React.createElement (object) => HTML(browser understand)
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hello world from React")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello world from React h1"),
+    React.createElement("h2", {}, "Hello world from React h2"),
+  ])
 );
 
 // const heading = React.createElement(
@@ -26,7 +26,7 @@ const parent = React.createElement(
 //   { id: "heading" },
 //   "Hello world from React !"
 // );
- console.log(parent); //this is an object not html tag yet !
+console.log(parent); //this is an object not html tag yet !
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
