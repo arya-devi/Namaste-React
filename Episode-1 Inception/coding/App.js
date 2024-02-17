920ef49f2d60f2599ae5fe0591540c5d9085d62d
Arya Devi <aryaarya96016@gmail.com>
2024-02-17 12:16:59 +0530

    Realized what happen when createElement and render method works

--- Episode-1 Inception/coding/App.js
+++ Episode-1 Inception/coding/App.js
@@ -1,3 +1,10 @@
-const heading = React.createElement("h1", {id : "heading"}, "Hello world from React !");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React !"
+);
+console.log(heading); //this is an object not html tag yet !
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(heading); // The render method take the react element that is a javascript object and it convert that into browser understands tags(HTML)
