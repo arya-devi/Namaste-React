7608ab29b6db9e164a1948c210aa8768fabfbbad
Arya Devi <aryaarya96016@gmail.com>
2024-02-21 13:35:19 +0530

    what about safty when using JSX

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1>Iam react Element going to Component</h1>;
-const heading1 = <h1>Iam react Element going to React Element</h1>;
+//this is an api call for attack us, so if the code is executed it will affected our safety
+const maliciousData = api.getData("malicious data");
 
 const ReactComponent = () => (
-  <>
-    <h1 className="heading">Iam Component going to Element</h1>
-    {heading}
-    <h2>
-      Basically these all are javascript.... react element becomes js object
-      .... jsx becomes react element and then js object....component is used to
-      write JSX and we can write js by adding {"{ }"} in component we can write
-      any javascript code 🚀
-    </h2>
-  </>
-);
-const heading2 = (
-  <div>
-    <ReactComponent />
-    {heading1}
-  </div>
-);
-
-const HeaderComponent2 = () => (
   <div id="container">
-    {heading2}
-
-    <h1 className="header">Iam Component going to Component</h1>
-  </div>
-);
-const HeaderComponent3 = () => (
-  <div id="container">
-    <HeaderComponent2 />
+    {maliciousData}
+    {/* dont be afraid JSX will take care of it , JSX is not executing tha data directly , JSX  will be automatically sanitize the data before execution,so no one can attack us */}
+    <h1 className="header">Iam Component</h1>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent3 />);
+root.render(<ReactComponent />);
