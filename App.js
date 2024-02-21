1735a1b7e269765a478598921f6bac41ce5d9591
Arya Devi <aryaarya96016@gmail.com>
2024-02-21 13:53:36 +0530

    how we can render or execute component in three ways

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//this is an api call for attack us, so if the code is executed it will affected our safety
-const maliciousData = api.getData("malicious data");
-
 const ReactComponent = () => (
   <div id="container">
-    {maliciousData}
-    {/* dont be afraid JSX will take care of it , JSX is not executing tha data directly , JSX  will be automatically sanitize the data before execution,so no one can attack us */}
     <h1 className="header">Iam Component</h1>
   </div>
 );
-
+const ReactComponent2 = () => (
+  <div id="container">
+    {/* these are the three ways to call the component.at the end of the day functional component is a normal javascript function because we know that JSX will be converted into normal javascript by babel */}
+    <ReactComponent />
+    {ReactComponent()}
+    <ReactComponent></ReactComponent>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<ReactComponent />);
