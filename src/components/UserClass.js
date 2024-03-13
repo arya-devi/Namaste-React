55f0cf9e04df59fb7083f3f2182686281354527f
Arya Devi <aryaarya96016@gmail.com>
2024-03-13 15:45:04 +0530

    how to create single and multiple state variable inside class component with using this.state,whenever we create an instance of a class the constructor is called, it is the best place to create state variable

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,17 +2,23 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
-    super(props); //In React components specifically, super(props) is used to pass props to the constructor of the parent class.
-    console.log(props);
+    super(props);
+    this.state = {
+      count1: 0,
+      count2: 1,
+    };
   }
- 
+
   render() {
-    const { name, location, email } = this.props;  //we can destructure the props and use it that component whenever you want 
+    const { name, location, email } = this.props;
+    const { count1, count2 } = this.state;
     return (
       <div className="user-card">
         <h1>{name}</h1>
         <h2>{location}</h2>
         <h3>{email}</h3>
+        <h1>count:{count1}</h1>
+        <h1>count:{count2}</h1>
       </div>
     );
   }
