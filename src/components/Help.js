8467e7062a502ae4cf01f163cfa57b171a3febd0
Arya Devi <aryaarya96016@gmail.com>
2024-03-13 20:37:19 +0530

    how lifecycle methods are called when there are multiple children

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,4 +1,4 @@
-//This is the parent component
+//This is the parent component consisting of mutiple children
 
 import React from "react";
 import UserClass from "./UserClass";
@@ -9,15 +9,20 @@ class Help extends React.Component {
     console.log("parent constructor"); //when this component render at first this constructor is called
   }
   componentDidMount() {
-    console.log("parent componentDidMount"); //when the child component is finished all the life cycle methods then only the componentdidmount is called
+    console.log("parent componentDidMount"); //when all the child components constructor,render and componentdidmount method is finished then only this parent componentdidmount is called
   }
   render() {
     console.log("parent render"); //then it consists of render method after the parent constructor is called then render will called
     return (
       <div className="user-details">
         <h2>User Details</h2>
-        <UserClass //Then it seen a class component  then it will goes to the class component
-          name={"Aryadevi"}
+        <UserClass              //Then it seen a class component then it will goes to that class component,in this class there are multiple child so after the first childs constructor and render method finished then it goes to the second child and repeat this
+          name={"first"}
+          location={"Kerala"}
+          email={"dewisdevelops@gmail.com"}
+        />
+        <UserClass 
+          name={"second"}     //second child
           location={"Kerala"}
           email={"dewisdevelops@gmail.com"}
         />
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -8,13 +8,22 @@ class UserClass extends React.Component {
     this.state = {
       count: 0,
     };
-    console.log("child constructor"); //when a class component load at first this constructor is called
+    console.log(this.props.name + "child constructor"); //when a class component load at first this constructor is called
   }
   componentDidMount() {
-    console.log("child componentDidMount"); //The componentdidMount helps to make API calls.after render method this componentdidMount will be called
+    console.log(this.props.name + "child componentDidMount"); //if there are multiple children in the parent class component this will called after the completion of all child constructor and render method
   }
+  //There are  three life cycle methods
+  //  1) Mounthing
+  //  2) Updating
+  //  3) Unmounting
+  // in react when the component is mounted , it is mounted in 2 phases
+  //  1) Render phace
+  //  {React is fast because of these phases}
+  //  2) Commit phase
+  //when the component is mounting , first the constructor is called then the render is called.This constructor and render comes under the Render phase, then the react updates the dom and then componentdidmount is called this is in the commit phase
   render() {
-    console.log("child render"); //once the constructor is called then the render is called
+    console.log(this.props.name + "child render"); //once the constructor is called then the render is called
     const { name, location, email } = this.props;
     const { count } = this.state;
     return (
