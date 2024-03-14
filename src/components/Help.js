b321403e71a5c3aea65768da273875d8a588cedc
Arya Devi <aryaarya96016@gmail.com>
2024-03-14 14:12:10 +0530

    how we can make Api call inside class component,Git Hub user Api Fetched

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,28 +1,17 @@
-//This is the parent component consisting of mutiple children
-
 import React from "react";
 import UserClass from "./UserClass";
 
 class Help extends React.Component {
   constructor() {
     super();
-    console.log("parent constructor"); //when this component render at first this constructor is called
   }
-  componentDidMount() {
-    console.log("parent componentDidMount"); //when all the child components constructor,render and componentdidmount method is finished then only this parent componentdidmount is called
-  }
+  componentDidMount() {}
   render() {
-    console.log("parent render"); //then it consists of render method after the parent constructor is called then render will called
     return (
       <div className="user-details">
         <h2>User Details</h2>
-        <UserClass              //Then it seen a class component then it will goes to that class component,in this class there are multiple child so after the first childs constructor and render method finished then it goes to the second child and repeat this
+        <UserClass
           name={"first"}
-          location={"Kerala"}
-          email={"dewisdevelops@gmail.com"}
-        />
-        <UserClass 
-          name={"second"}     //second child
           location={"Kerala"}
           email={"dewisdevelops@gmail.com"}
         />
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,55 +1,40 @@
-//This is the child component
-
+import { json } from "body-parser";
 import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Dummy",
+        location: "dummy",
+        avatar_url: "",
+        bio: "nop",
+      },
     };
-    console.log(this.props.name + "child constructor"); //when a class component load at first this constructor is called
   }
-  componentDidMount() {
-    console.log(this.props.name + "child componentDidMount"); //if there are multiple children in the parent class component this will called after the completion of all child constructor and render method
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/arya-devi");
+    const json = await data.json();
+    console.log(json);
+    this.setState({
+      userInfo: json,
+    });
   }
-  //There are  three life cycle methods
-  //  1) Mounthing
-  //  2) Updating
-  //  3) Unmounting
-  // in react when the component is mounted , it is mounted in 2 phases
-  //  1) Render phace
-  //  {React is fast because of these phases}
-  //  2) Commit phase
-  //when the component is mounting , first the constructor is called then the render is called.This constructor and render comes under the Render phase, then the react updates the dom and then componentdidmount is called this is in the commit phase
+
   render() {
-    console.log(this.props.name + "child render"); //once the constructor is called then the render is called
-    const { name, location, email } = this.props;
-    const { count } = this.state;
+    const { name, location, avatar_url, bio } = this.state.userInfo;
+
     return (
       <div className="user-card">
-        <h1>{name}</h1>
-        <h2>{location}</h2>
-        <h3>{email}</h3>
-        <h1>count:{count}</h1>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count - 1,
-            });
-          }}
-        >
-          decrement
-        </button>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Increment
-        </button>
+        <div>
+          <h1>{bio}</h1>
+          <h2>{name}</h2>
+          <h3>{location}</h3>
+        </div>
+        <div>
+          <img src={avatar_url} alt="" />
+        </div>
       </div>
     );
   }
