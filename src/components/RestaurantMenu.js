519a23a4a8234f404f5d4d637476de0055aa5095
Arya Devi <aryaarya96016@gmail.com>
2024-03-30 19:29:59 +0530

    How to do at a time only one accordion will expand with the help of parent,controlled and uncontrolled components,Lifting the state up

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,3 +1,130 @@
+// import { useEffect, useState } from "react";
+// import Shimmer from "./Shimmer";
+// import { useParams } from "react-router-dom";
+// import useRestaurantData from "../utils/useRestaurantMenu";
+// import MenuCard from "./MenuCard";
+// import MenuCategory from "./MenuCategory";
+
+// const RestaurentMenu = () => {
+//   const [vegOnly, setVegOnly] = useState(false);
+//   const [resMenu, setResMenu] = useState([]);
+
+//   const { id } = useParams();
+//   const resInfo = useRestaurantData(id);
+
+//   useEffect(() => {
+//     if (resInfo) {
+//       const totalData =
+//         resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+//       const totalMenu = totalData.filter((item) => item?.card?.card?.itemCards);
+
+//       setResMenu(totalMenu);
+//       console.log(totalMenu);
+
+//       console.log(resMenu);
+//       console.log(resInfo);
+//     }
+//   }, [resInfo]);
+
+//   if (resInfo === null) {
+//     return <Shimmer />;
+//   }
+//   const {
+//     areaName,
+//     avgRating,
+//     city,
+//     costForTwoMessage,
+//     cuisines,
+//     name,
+//     totalRatingsString,
+//   } = resInfo?.cards[2]?.card?.card?.info;
+//   const { slaString, lastMileTravelString } =
+//     resInfo?.cards[2]?.card?.card?.info?.sla;
+
+//   const offers =
+//     resInfo?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.offers;
+
+//   // console.log(offers);
+
+//   return (
+//     <div className="menu-container">
+//       <div className="res-details">
+//         <div className="res-place">
+//           <h5>
+//             Home/{city}/{name}
+//           </h5>
+//         </div>
+//         <div className="res-namecontainer">
+//           <div className="res-name">
+//             <h2>{name}</h2>
+//             <p>{cuisines.join(" , ")}</p>
+//             <p>
+//               {areaName}-{lastMileTravelString}
+//             </p>
+//           </div>
+//           <div className="res-rating">
+//             <div className="rate-star">
+//               <i className="bx bxs-star"></i>
+//               <h5>{avgRating}</h5>
+//             </div>
+//             <div className="rating">
+//               <h5>{totalRatingsString}</h5>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="time-and-cost">
+//           <div className="time">
+//             <h5>
+//               <i className="bx bx-time-five"></i> {slaString}
+//             </h5>
+//           </div>
+//           <div className="cost">
+//             <h5>{costForTwoMessage}</h5>
+//           </div>
+//         </div>
+//         <div className="offer-card">
+//           {offers.map((offer, index) => (
+//             <div key={index} className="res-offers">
+//               <h4>
+//                 <span>
+//                   {" "}
+//                   <i className="bx bxs-offer bx-flashing"></i>
+//                 </span>
+
+//                 {offer.info.header}
+//               </h4>
+//               <h5>
+//                 {offer.info.couponCode} | {offer.info.description}
+//               </h5>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//       <div className="menu-details">
+//         <div className="veg-only">
+//           <h4>Veg only</h4>
+//           <div className={`veg-only-btn ${vegOnly ? "active" : ""}`}>
+//             <i
+//               onClick={() => setVegOnly((prev) => !prev)}
+//               className="bx bx-food-tag"
+//             ></i>
+//           </div>
+//         </div>
+
+//         {vegOnly
+//           ? resMenu.map((item) => <MenuCard key={item.id} item={item} />)
+//           : resMenu.map((category, index) => (
+//               <MenuCategory
+//                 key={category.id}
+//                 category={category}
+//                 index={index}
+//               />
+//             ))}
+//       </div>
+//     </div>
+//   );
+// };
+// export default RestaurentMenu;
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
@@ -8,6 +135,7 @@ import MenuCategory from "./MenuCategory";
 const RestaurentMenu = () => {
   const [vegOnly, setVegOnly] = useState(false);
   const [resMenu, setResMenu] = useState([]);
+  const [showIndex, setShowIndex] = useState(null);
 
   const { id } = useParams();
   const resInfo = useRestaurantData(id);
@@ -114,10 +242,14 @@ const RestaurentMenu = () => {
         {vegOnly
           ? resMenu.map((item) => <MenuCard key={item.id} item={item} />)
           : resMenu.map((category, index) => (
+            // now this is a controlled component the parent(RestaurantMenu) is controlling the child(MenuCategory) its doest have its own state
               <MenuCategory
                 key={category.id}
                 category={category}
-                index={index}
+                show={index === showIndex ? true : false}
+                setShowIndex={() =>
+                  setShowIndex(index === showIndex ? null : index)
+                }
               />
             ))}
       </div>
